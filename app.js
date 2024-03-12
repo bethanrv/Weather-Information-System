533fd5f73343666c258918c4b2ce3f0938041cb4
Ethan Rodgers Vargo <rodgersvargoe@gmail.com>
2024-03-12 14:19:12 -0400

    add node js request to openWeatherData & weather endpoint error handling

--- app.js
+++ app.js
@@ -4,30 +4,43 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
-// Weather get endpoint 
-app.get('/weather', (req, res) => {
+/* Weather get endpoint
+    @Params: City
+    @Returns: JSON weather data 
+    @Description: 
+    - Use OpenWeatherMap weather api with given city to get weather data as json
+*/
+app.get('/weather', async (req, res) => {
   const city = req.query.city;
   if ( !city ) // validate city param
-    return res.status(400).send('Parameter {city} is missing.')
-  res.send('test');
+    return res.status(400).send('Parameter {city} is missing')
+  
+  // attempt retrieve weather json data - report error code and messsage on fail
+  try { 
+    const cityWeatherData = await getWeatherByCity(city)
+    res.send(cityWeatherData);
+  } catch (error) {
+    console.error('Error fetching weather data:', error.response ? error.response.data : error.message);
+    res.status(500).send('Error fetching weather data: ' + error.response ? error.response.data : error.message)
+  }
 });
+
+// Request to OpenWeatherMap weather api given city
+const getWeatherByCity = async (city) => {
+  try{
+    const cityWeatherResponse = await axios.get(process.env.Weather_API_URL, {
+        params: {
+            q: city,
+            appid: process.env.API_KEY
+        }
+    });
+    return cityWeatherResponse.data
+  } catch (error) {
+    throw error
+  }
+}
 
 // Start server on port 3000
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`)
 });
-
-// Request to OpenWeatherMap api
-const getWeatherByCity = (city) => {
-  /*
-    example call:
-        http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
-  */
-  axios.get(process.env.API_URL)
-  .then(response => {
-      console.log('API Response:', response.data);
-  })
-  .catch(error => {
-      console.error('Error fetching data:', error);
-  });
-}
